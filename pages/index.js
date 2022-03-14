f736836dbb2334f143e8de09aca9bdbdbcc34d96
batuhan içöz <batuhan@icoz.co>
2022-03-14 19:29:25 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
 
         <p>we invest time & money into subjectively interesting ideas like:</p>
         <ul>
-          <li><a href="https://www.komunite.com.tr" alt="Komünite">Komünite</a>, a venture builder fund for independent creators</li>
+          <li><a href="https://www.komunite.com.tr" alt="Komünite">Komünite</a>, a venture builder fund for independent creators (<a href="https://webrazzi.com/2021/11/30/yaraticilari-ve-mikro-girisimcileri-fonlayan-komunite-140-bin-dolar-yatirim-aldi/">press release</a>)</li>
           <li><a href="https://www.saltana.com" alt="Saltana">Saltana</a>, a toolkit for digital creators</li>
           <li>a stealth project focusing on animation and story telling</li>
         </ul>
